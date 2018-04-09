5dd25ed35e4b38454ff3c068f780fa55c1493a0a
Dandelion Mané <dl@dandelion.io>
2018-04-09 22:21:41 +0300

    Add a Transaction datatype.
    It is a standard way of representing acquiring or disposing of some
    cryptoasset. It uses bigjs arbitrary-precision numeric representations
    for the cryptocurrency amount and price.
    
    There are a number of transaction types, such as "TRADE", "FEE", and
    "DIV". Each transaction type is associated with a TransactionTypeInfo,
    with information on the tax implications of that kind of transaction.
    
    Test plan:
    The tests thus far all focus on typechecking. Observe that `yarn flow`
    and `yarn test` both pass at present.
    
    If you uncomment the invalid transaction in transaction.test.js, then
    `yarn flow` produces errors. Also, if you make the transactionTypes not
    all map to a TransactionTypeInfo (e.g. change isCapitalGains to "foo"
    for some tx type), then `yarn flow` fails in the static assertion.

---
+++ src/core/transaction.test.js
@@ -0,0 +1,43 @@
+// @flow
+
+import moment from "moment";
+import Big from "big.js";
+
+import type {Transaction} from "./transaction";
+
+describe("transaction typechecking", () => {
+  it("can construct transaction for various transaction types", () => {
+    // Not intended to be exhaustive, just verifying that the element types work
+    const trade: Transaction = {
+      price: Big(1),
+      amount: Big(2),
+      ticker: "FOO",
+      date: moment(),
+      type: "TRADE",
+    };
+    const gift: Transaction = {
+      price: Big(1),
+      amount: Big(2),
+      ticker: "FOO",
+      date: moment(),
+      type: "GIFT",
+    };
+    const fork: Transaction = {
+      price: Big(1),
+      amount: Big(2),
+      ticker: "FOO",
+      date: moment(),
+      type: "FORK",
+    };
+  });
+  //  it("invalid transaction types produce flow errors", () => {
+  //    // if uncommented, this test produces a flow error
+  //    const bad: Transaction = {
+  //      price: Big(1),
+  //      amount: Big(2),
+  //      ticker: "FOO",
+  //      date: moment(),
+  //      type: "BAD",
+  //    };
+  //  });
+});
