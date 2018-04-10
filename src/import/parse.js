d046cf1bf7b3eddcbe1dfbfaac4ed08568b35630
Dandelion Mané <dl@dandelion.io>
2018-04-10 22:07:58 +0300

    Add `parseNumber` and `parseDate`
    parseNumber parses a number or string into a Big, removing dollar signs
    or commas as appropriate.
    
    parseDate parses a slash or dash delimited date string into a moment.
    
    Test plan:
    - Verify that tests still pass

---
+++ src/import/parse.js
@@ -0,0 +1,29 @@
+// @flow
+
+import moment from "moment";
+import Big from "big.js";
+
+export function parseNumber(x: string | number) {
+  if (typeof x === "number") {
+    return Big(x);
+  }
+  if (typeof x !== "string") {
+    throw new Error(`parseNumber: unexpected type ${x}`);
+  }
+  if (x[0] === "$") {
+    x = x.substr(1);
+  }
+  const stripped = x.replace(/,/g, "");
+  const val = Big(stripped);
+  return val;
+}
+
+export function parseDate(x: string) {
+  x = x.replace(/\//g, "-");
+  if (!x.match(/\d+-\d+-\d+/)) {
+    throw new Error(`${x} doesn't look like a date`);
+  }
+  const date = moment(x, "MM-DD-YYYY");
+
+  return date;
+}
---
+++ src/import/parse.test.js
@@ -0,0 +1,57 @@
+// @flow
+
+import moment from "moment";
+import Big from "big.js";
+
+import {parseNumber, parseDate} from "./parse";
+
+describe("parseNumber", () => {
+  it("handles simple numeric cases", () => {
+    expect(parseNumber(0.1)).toEqual(Big("0.1"));
+    expect(parseNumber(5)).toEqual(Big("5"));
+  });
+
+  it("handles simple string cases", () => {
+    expect(parseNumber("0.1")).toEqual(Big("0.1"));
+    expect(parseNumber("5")).toEqual(Big("5"));
+  });
+  it("handles strings with dollar signs", () => {
+    expect(parseNumber("$50")).toEqual(Big("50"));
+  });
+  it("handles strings with commas", () => {
+    expect(parseNumber("50,000")).toEqual(Big("50000"));
+    expect(parseNumber("50,000,000.00")).toEqual(Big("50000000"));
+  });
+  it("errors on bad inputs", () => {
+    expect(() => parseNumber("500btc")).toThrow();
+    expect(() => parseNumber("50$0")).toThrow();
+    expect(() => parseNumber("")).toThrow();
+    //$ExpectError
+    expect(() => parseNumber(true)).toThrow();
+    //$ExpectError
+    expect(() => parseNumber(null)).toThrow();
+  });
+});
+
+describe("parseDate", () => {
+  function momentOnDate(year, month, day) {
+    return moment().set({
+      year,
+      month: month - 1,
+      date: day,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: 0,
+    });
+  }
+  it("parses slash dates", () => {
+    expect(parseDate("04/03/2015").isSame(momentOnDate(2015, 4, 3))).toBe(true);
+  });
+  it("parses dash dates", () => {
+    expect(parseDate("04-03-2015").isSame(momentOnDate(2015, 4, 3))).toBe(true);
+  });
+  it("errors on bad dates", () => {
+    expect(() => parseDate("")).toThrow();
+  });
+});
